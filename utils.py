294fae6da83ef1be785f232c6515f1894ed4c176
Taylor Archibald <36053181+Tahlor@users.noreply.github.com>
2018-03-27 10:14:57 -0600

    detect EOL chars

--- utils.py
+++ utils.py
@@ -6,16 +6,16 @@ from six.moves import cPickle
 import numpy as np
 import re
 import itertools
-
+    
 from gensim.summarization.summarizer import summarize
 
-
 class TextLoader():
-    def __init__(self, data_dir, batch_size, seq_length, encoding=None):
+    def __init__(self, data_dir, batch_size, seq_length, encoding=None, simple_vocab = True):
         self.data_dir = data_dir
         self.batch_size = batch_size
         self.seq_length = seq_length
-
+        self.simple_vocab = simple_vocab
+        
         input_file = os.path.join(data_dir, "input.txt")
         vocab_file = os.path.join(data_dir, "vocab.pkl")
         tensor_file = os.path.join(data_dir, "data.npy")
@@ -35,17 +35,21 @@ class TextLoader():
         Tokenization/string cleaning for all datasets except for SST.
         Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data
         """
-        string = re.sub(r"[^가-힣A-Za-z0-9(),!?\'\`]", " ", string)
-        string = re.sub(r"\'s", " \'s", string)
-        string = re.sub(r"\'ve", " \'ve", string)
-        string = re.sub(r"n\'t", " n\'t", string)
-        string = re.sub(r"\'re", " \'re", string)
-        string = re.sub(r"\'d", " \'d", string)
-        string = re.sub(r"\'ll", " \'ll", string)
+        string = re.sub(r"\n\s*", r" | ", string)
+        string = re.sub(r"[^|^가-힣A-Za-z0-9(),!?\'\`]", " ", string)
+        #string = re.sub(r"\'s", " \'s", string)
+        #string = re.sub(r"\'ve", " \'ve", string)
+        #string = re.sub(r"n\'t", " n\'t", string)
+        #string = re.sub(r"\'re", " \'re", string)
+        #string = re.sub(r"\'d", " \'d", string)
+        #string = re.sub(r"\'ll", " \'ll", string)
+        string = re.sub(r"[0-9]+", "  ", string)
         string = re.sub(r",", " , ", string)
         string = re.sub(r"!", " ! ", string)
         string = re.sub(r"\(", " \( ", string)
         string = re.sub(r"\)", " \) ", string)
+        string = re.sub(r";", " ; ", string)
+        string = re.sub(r":", " : ", string)
         string = re.sub(r"\?", " \? ", string)
         string = re.sub(r"\s{2,}", " ", string)
         return string.strip().lower()
@@ -55,6 +59,7 @@ class TextLoader():
         Builds a vocabulary mapping from word to index based on the sentences.
         Returns vocabulary mapping and inverse vocabulary mapping.
         """
+
         # Build vocabulary
         word_counts = collections.Counter(sentences)
         # Mapping from index to word
@@ -69,14 +74,25 @@ class TextLoader():
             data = f.read()
 
         # Optional text cleaning or make them lower case, etc.
-        #data = self.clean_str(data)
-        x_text = data.split()
+        if self.simple_vocab:
+            data = self.clean_str(data)        
 
+        # This is not optimized, shouldn't be too bad though
+        x_text = ["\n" if i == "|" else i for i in data.split()]
+        #x_text = data.split()
         self.vocab, self.words = self.build_vocab(x_text)
         self.vocab_size = len(self.words)
 
         with open(vocab_file, 'wb') as f:
             cPickle.dump(self.words, f)
+        
+        """print(data[0:500])
+        # print(data1[0:500])
+        print(x_text[0:500])
+        print("\n" in self.vocab)
+        print([x for x in self.vocab if "\n" in x])
+        Stop
+        """
 
         #The same operation like this [self.vocab[word] for word in x_text]
         # index of words as our basic data
@@ -105,32 +121,58 @@ class TextLoader():
 
         ydata[:-1] = xdata[1:]
         ydata[-1] = xdata[0]
-        
+
+        # Find end of line mark index
+        #print(self.vocab)
+        #print(self.words)
+
+        self.endline_idx = self.vocab['\n']
+
+        # Find synonym of endline words in corpus
+
         # Create sequence of vocab indices (inputs, outputs)
-        # Find the word before newline; give this word for every word in that line; occasionally give it a synonym
+        # Giant list - [Steps in Epoch, Batch size, Sequence size]
+        
         self.x_batches = np.split(xdata.reshape(self.batch_size, -1), self.num_batches, 1)
-        print(x_batches)
-        Stop
         self.y_batches = np.split(ydata.reshape(self.batch_size, -1), self.num_batches, 1)
+        self.get_last_words()
+
+    def get_last_words(self):
+        self.last_words = np.copy(self.x_batches)
+        for batch in self.last_words:
+            for element in batch:
+                last_word = element[::-1][0]
+                for n, word in enumerate(element[::-1]):
+                    if word == self.endline_idx and n + 1 < len(element):
+                        last_word = self.get_non_symbol(element[::-1][n+1:])
+                    element[::-1][n] = last_word
+                #print(element)
+                #print(np.asarray(self.words)[element])
+        #print(np.asarray(self.words)[self.last_words[0][0]])
+        #print(np.asarray(self.x_batches).shape)
+
+    def get_non_symbol(self, l):
+        for el in l:
+            if self.words[el].isalpha():
+                return el
+
+        # If not found return empty line
+        return self.endline_idx
 
     #X is input, Y is output
     def next_batch(self):
-        x, y = self.x_batches[self.pointer], self.y_batches[self.pointer]
+        x, y, last_words = self.x_batches[self.pointer], self.y_batches[self.pointer], self.last_words[self.pointer]
         self.pointer += 1
-        return x, y
+        return x, y, last_words
 
     def reset_batch_pointer(self):
         self.pointer = 0
 
-        
     def get_last_word():
-        
+        pass
         
     def get_sentiment():
         pass
         
     def get_summary():
         gensim.summarization.summarizer.summarize(text, ratio=0.2, word_count=None, split=False)
-
-    
-    
